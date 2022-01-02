8cb4cd1a6fe6f0e53418ae541f80202e405d73f3
Grayd-WebDev <grayd2@yandex.ru>
2022-01-02 14:40:56 +0300

    The server has been ran and connected to mongoose.

---
+++ server/index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+
+const { PORT, DB_URL } = process.env;
+
+const app = express();
+
+app.use(cors());
+app.use(cookieParser());
+
+const start = async () => {
+  try {
+    await mongoose.connect(DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    app.listen(PORT, () => {
+      console.log(`The server is working on port ${PORT}`);
+    });
+  } catch (e) {
+    console.log("The Error happened in start of the server, it says -", e);
+  }
+};
+
+start();
